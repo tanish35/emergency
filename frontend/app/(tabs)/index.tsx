101bae9ad3f4b088b6fd9c937d76be4e393a27ea
tanish35 <tanishmajumdar2912@gmail.com>
2025-02-08 12:14:25 +0530

    Added ambulance live location backend and frontend

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import HomeScreen from "../../components/HomeScreen";
 
-console.log("App.tsx is loaded!"); // Debugging
+console.log("App.tsx is loaded!");
 
 export default function App() {
   console.log("Rendering App.tsx...");
